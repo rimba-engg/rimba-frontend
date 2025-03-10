0656eb69ad583c5562f8479963e3980e2c99417c
akshay-rimba <akshay@rimba.ai>
2025-03-10 17:22:13 +0000

    add hotjar back

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={rubik.className}>
+        {process.env.NODE_ENV === 'production' && (
+          <Script
+            id="hotjar-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function (c, s, q, u, a, r, e) {
+                    c.hj=c.hj||function(){(c.hj.q=c.hj.q||[]).push(arguments)};
+                    c._hjSettings = { hjid: a };
+                    r = s.getElementsByTagName('head')[0];
+                    e = s.createElement('script');
+                    e.async = true;
+                    e.src = q + c._hjSettings.hjid + u;
+                    r.appendChild(e);
+                })(window, document, 'https://static.hj.contentsquare.net/c/csq-', '.js', 5321437);
+              `,
+            }}
+          />
+        )}
         {children}
       </body>
     </html>
