8559dc17b4b29e03ef7a06e5796087e02a2bcb32
akshay-rimba <akshay@rimba.ai>
2025-03-27 01:43:42 +0000

    update analytics tab for novilla

--- app/(auth)/reporting/analytics/page.tsx
+++ app/(auth)/reporting/analytics/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { FloatingLabelInput } from '@/components/ui/floating-label-input';
 import { api } from '@/lib/api';
 import {
   Chart as ChartJS,
@@ -11,11 +12,13 @@ import {
   Tooltip,
   Legend
 } from 'chart.js';
+import { DateTime } from 'luxon';
 import { Loader2 } from 'lucide-react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import { ToastContainer, toast } from 'react-toastify';
+import QueryTable from '@/components/table/QueryTable';
 
 // Register ChartJS components
 ChartJS.register(
@@ -34,29 +37,45 @@ const options = {
       legend: {
         position: 'top' as const,
       },
-      title: {
-        display: true,
-        text: 'Kurz to Sioux Center Balance',
-      },
     },
   };
 
 interface AnalyticsResponse {
     chart_config: any;
+    table_data: any;
 }
 
 export default function AnalyticsPage() {
     const [chartConfig, setChartConfig] = useState<any>(null);
     const [loading, setLoading] = useState(true);
+    const [startDate, setStartDate] = useState<string>(DateTime.now().setZone('America/New_York').minus({ weeks: 1 }).toISO()?.slice(0, 10) ?? '');
+    const [endDate, setEndDate] = useState<string>(DateTime.now().setZone('America/New_York').toISO()?.slice(0, 10) ?? '');
+    const [rowData, setRowData] = useState<any>([]);
+    const [columnDefs, setColumnDefs] = useState<any>([]);
+    const [relative, setRelative] = useState('Select time range...');
     
     useEffect(() => {
         fetchAnalytics();
-    }, []);
+    }, [startDate, endDate]);
 
     const fetchAnalytics = () => {
         setLoading(true);
-        api.post<AnalyticsResponse>('/reporting/v2/rng/analytics/', {})
-            .then(data => setChartConfig(data.chart_config))
+        api.post<AnalyticsResponse>('/reporting/v2/rng/analytics/', {
+            start_date: startDate,
+            end_date: endDate
+        })
+            .then(data => {
+                setChartConfig(data.chart_config);
+                setRowData(data.table_data);
+                
+                // set column defs
+                setColumnDefs(Object.keys(data.table_data[0]).map((key: any) => {
+                    return {
+                        headerName: key,
+                        field: key
+                    }
+                }));
+            })
             .catch(error => {
                 console.error('Error fetching analytics data:', error);
                 toast.error('Error fetching analytics data');
@@ -76,9 +95,88 @@ export default function AnalyticsPage() {
       }
 
     return (
-        <div className="w-full h-[500px] p-4">
+      <div className='space-y-4'>
+        <div className='text-2xl font-bold'>Analytics</div>
+
+        <div className="flex gap-6">
+          <div className="flex flex-col p-4 gap-4 w-1/4">
+            <div className="flex flex-row gap-4">
+              <FloatingLabelInput
+                label="Start Day (EST)"
+                type="date" 
+                className="w-full"
+                max={endDate}
+                value={startDate}
+                onChange={(e) => setStartDate(e.target.value)}
+              />
+              <FloatingLabelInput
+                label="End Day (EST)"
+                type="date"
+                className="w-full"
+                min={startDate}
+                value={endDate}
+                onChange={(e) => setEndDate(e.target.value)}
+              />
+            </div>
+
+            <div>OR</div>
+
+            <div>
+              <select 
+                className="px-4 py-2 border rounded"
+                onChange={(e) => {
+                  const [unit, amount] = e.target.value.split('-');
+                  setRelative(`Last ${amount} ${unit}`);
+                  const now = DateTime.now().setZone('America/New_York');
+                  let start;
+                  let end = now;
+
+                  switch(unit) {
+                    case 'day':
+                      start = now.minus({ days: parseInt(amount) });
+                      break;
+                    case 'week':
+                      start = now.minus({ weeks: parseInt(amount) });
+                      break;
+                    case 'month':
+                      start = now.minus({ months: parseInt(amount) });
+                      break;
+                  }
+
+                  setStartDate(start?.toISO()?.slice(0, 10) ?? '');
+                  setEndDate(end?.toISO()?.slice(0, 10) ?? '');
+                }}
+              >
+                <option value="">{relative}</option>
+                <optgroup label="Days">
+                  {[1,2,3,4,5,6].map(n => (
+                    <option key={`day-${n}`} value={`day-${n}`}>Last {n} day{n > 1 ? 's' : ''}</option>
+                  ))}
+                </optgroup>
+                <optgroup label="Weeks">
+                  {[1,2,3,4].map(n => (
+                    <option key={`week-${n}`} value={`week-${n}`}>Last {n} week{n > 1 ? 's' : ''}</option>
+                  ))}
+                </optgroup>
+                <optgroup label="Months">
+                  {[1,2,3].map(n => (
+                    <option key={`month-${n}`} value={`month-${n}`}>Last {n} month{n > 1 ? 's' : ''}</option>
+                  ))}
+                </optgroup>
+              </select>
+            </div>
+          </div>
+
+          <div className="w-3/4 h-[300px]">
             <Line options={options} data={chartConfig} />
             <ToastContainer />
+          </div>
         </div>
+        
+        <QueryTable
+          initialRowData={rowData}
+          initialColumnDefs={columnDefs}
+        />
+      </div>
     );
 }
